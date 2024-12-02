6e9ec713ff35c803fb541dc41971671c3a8ffadc
mattchristenson <matt@rcc.team>
2024-12-02 12:25:04 -0600

    latest

--- wb8demo2/src/main/java/com/pluralsight/Main.java
+++ wb8demo2/src/main/java/com/pluralsight/Main.java
@@ -5,9 +5,18 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args){
 
+        if (args.length != 2) {
+            System.out.println(
+                    "Application needs two arguments to run: " +
+                            "java com.pluralsight.wb8demo2 <username> <password>");
+            System.exit(1);
+        }
 
-       try{
+        // get the user name and password from the command line args
+        String username = args[0];
+        String password = args[1];
 
+        try{
 
            // load the MySQL Driver
            Class.forName("com.mysql.cj.jdbc.Driver");
@@ -15,10 +24,8 @@ public class Main {
             // 1. open a connection to the database
             // use the database URL to point to the correct database
            Connection connection;
-           connection = DriverManager.getConnection(
-                   "jdbc:mysql://localhost:3306/sakila",
-                   "root",
-                   "yearup");
+
+           connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila", username, password);
 
            int country_id = 103;
 
@@ -29,10 +36,15 @@ public class Main {
            ResultSet results = pStatement.executeQuery();
             // process the results
            while (results.next()) {
+               int cityId = results.getInt("city_id");
                String city = results.getString("city");
+               System.out.println(cityId);
                System.out.println(city);
+
            }
             // 3. Close the connection
+           results.close();
+           pStatement.close();
            connection.close();
 
        } catch (ClassNotFoundException e) {
