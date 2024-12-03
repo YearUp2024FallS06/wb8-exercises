10c1ddd90579f07a841fb0691aea2aec5ba64f50
mattchristenson <matt@rcc.team>
2024-12-03 14:26:10 -0600

    nw 4

--- NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,8 @@
 package com.pluralsight;
 
+import com.mysql.cj.x.protobuf.MysqlxPrepare;
+import com.sun.jdi.ClassNotPreparedException;
+
 import java.sql.*;
 
 
@@ -22,25 +25,99 @@ public class Main {
                 Please select from the following choices:
                 1 - Display All Products
                 2 - Display All Customers
+                3 - Display All Categories
                 0 - Exit
                 >>>\s""";
 
         int selection;
 
         // User Interface Loop
-        do {
-            selection = Console.PromptForInt(options);
-            switch (selection) {
-                case 1 -> processDisplayAllProducts(username, password);
-                case 2 -> processDisplayAllCustomers(username, password);
-                case 0 -> System.exit(0);
-                default -> System.out.println("Invalid selection. Please try again.");
+        try{
+            do {
+                selection = Console.PromptForInt(options);
+                switch (selection) {
+                    case 1 -> processDisplayAllProducts(username, password);
+                    case 2 -> processDisplayAllCustomers(username, password);
+                    case 3 -> processDisplayAllCategories(username, password);
+                    case 0 -> System.exit(0);
+                    default -> System.out.println("Invalid selection. Please try again.");
+                }
+            } while (true);
+        }
+
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+
+
+
+    }
+
+    private static void processDisplayAllCategories(String username, String password) {
+
+
+        // 1. open a connection to the database
+        // use the database URL to point to the correct database
+
+        try (Connection  connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);)
+        {
+
+            try(
+                    PreparedStatement statement = connection.prepareStatement("SELECT CategoryId, CategoryName FROM Categories");
+                    ResultSet results = statement.executeQuery();
+            )
+            {
+
+                displayCategoriesHeader();
+                // process the results
+                while (results.next()) {
+                    displayCategoriesRow(results.getInt(1),results.getString(2) );
+                }
+
             }
-        } while (true);
+
+            int selectedCategoryId = Console.PromptForInt("Please enter a Category ID: ");
+
+            try(PreparedStatement statement = connection.prepareStatement("SELECT * FROM Products WHERE CategoryId = ?"))
+            {
+                statement.setInt(1, selectedCategoryId);
+
+                try(ResultSet results = statement.executeQuery()){
+
+                    displayProductsHeader();
+                    // process the results
+                    while (results.next()) {
+
+                        displayProductsRow(
+                                results.getInt(1),
+                                results.getString(2),
+                                results.getDouble(3),
+                                results.getInt(4)
+                        );
 
 
+                    }
+                }
+            }
 
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    private static void displayCategoriesRow(int categoryId, String categoryName) {
+        System.out.printf("%10d %-40s \n", categoryId, categoryName);
+    }
+
+    private static void displayCategoriesHeader() {
+        System.out.printf("%-10s %-40s \n", "Id", "Name");
     }
 
     private static void processDisplayAllCustomers(String username, String password) {
