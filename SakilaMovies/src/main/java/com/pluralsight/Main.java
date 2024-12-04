1d4b0e0af9b9ce08565894017b3936fc60ac1037
mattchristenson <matt@rcc.team>
2024-12-04 13:32:17 -0600

    finish sakila

--- SakilaMovies/src/main/java/com/pluralsight/Main.java
+++ SakilaMovies/src/main/java/com/pluralsight/Main.java
@@ -1,54 +1,60 @@
 package com.pluralsight;
 
+import com.pluralsight.models.*;
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import javax.sql.DataSource;
 import java.sql.*;
+import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
 
-
-        String username = args[0];
-        String password = args[1];
-
         String lastName = Console.PromptForString("What is the last name of an actor you like? ");
 
-        try(BasicDataSource dataSource = new BasicDataSource();){
+        try(BasicDataSource dataSource = getConfiguredDataSource(args);){
 
-            dataSource.setUrl("jdbc:mysql://localhost:3306/sakila");
-            dataSource.setUsername(username);
-            dataSource.setPassword(password);
+            SakilaDataManager dm = new SakilaDataManager(dataSource);
 
-            //do the stuff with the datasource here...
-
-            try(Connection connection = dataSource.getConnection();){
-                try(PreparedStatement actorsByLastNameQuery = connection.prepareStatement(
-                        "SELECT * FROM Actor WHERE last_name = ?");)
-                {
-                    actorsByLastNameQuery.setString(1, lastName);
-                    try(ResultSet results = actorsByLastNameQuery.executeQuery())
-                    {
-                        boolean found = false;
-                        while(results.next()){
-                            found = true;
-                            System.out.printf("%d - %s %s\n",
-                                    results.getInt(1),
-                                    results.getString(2),
-                                    results.getString(3));
-                        }
-                        if(!found){
-                            System.out.println("No actors with that name.");
-                        }
-                    }
+            List<Actor> actors = dm.actorsByLastName(lastName);
+            if(!actors.isEmpty()){
+                for(Actor a : actors){
+                    System.out.println(a);
                 }
 
+                int actorId = Console.PromptForInt("Please enter an actor ID: ");
+
+                List<Film> films = dm.filmsByActorId(actorId);
+
+                for(Film f : films){
+                    System.out.println(f);
+                }
+
+            }
+            else
+            {
+                System.out.println("No actors found.");
             }
 
         }
         catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
+    }
+
+
+    public static BasicDataSource getConfiguredDataSource(String[] args){
+        String username = args[0];
+        String password = args[1];
+        String sqlServerUrl = args[2];
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(sqlServerUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
 
 
     }
